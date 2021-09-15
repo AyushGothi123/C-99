f204b7bc349aed022bfc0779791053cd783b19b4
AyushGothi123 <72344991+AyushGothi123@users.noreply.github.com>
2021-09-15 18:40:23 +0530

    Add files via upload

---
+++ readfile.py
@@ -0,0 +1,13 @@
+'''import os
+with open("sample1.txt") as f:
+    flines = f.readlines()
+
+for line in flines:
+    print(line)
+
+path = r"C:\Project C-98\sample1.txt"
+split = os.path.splitext(path)
+print(split[1])'''
+
+str ="python"
+print(str[3])
